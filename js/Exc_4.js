fab8590bc4e741c3448d00032418c017f582e365
Liliana <lilyfono21@gmail.com>
2024-05-14 21:13:25 -0700

    Exc_4

---
+++ js/Exc_4.js
@@ -0,0 +1,14 @@
+let a = 1
+let b = 3
+let c = 5
+
+if (a + b > c){
+  if (a + c > b){
+    if (b + c > a){
+        console.log('It is a triangle')
+    }
+ }
+}
+else{
+    console.log('It is not a triangle')
+}
