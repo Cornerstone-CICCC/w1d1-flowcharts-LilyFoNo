bf2fb8b7bcfe4507ff992caf44c256f2faf2e8a1
Liliana <lilyfono21@gmail.com>
2024-05-14 21:04:22 -0700

    Exc_3

---
+++ js/Exc_3.js
@@ -0,0 +1,15 @@
+let a = 1
+let b = 6
+let c = 22
+
+if (a > b && a > c){
+ console.log('The largest number is ',a)
+}
+else {
+    if (b > c) {
+        console.log('The largest number is ',b)
+    }
+    else {
+        console.log('The largest number is ',c)
+    }
+}
